0ed40c8ac99489abd57e0d7e2eb123aea7ea3396
rabia çakıcı <rabia.cakiciiii@gmail.com>
2025-07-28 13:25:16 +0300

    son

---
+++ LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  AnimalMatchGame
+//
+//  Created by Rabia Çakıcı on 26.07.2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
